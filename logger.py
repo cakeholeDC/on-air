9179453bbe9d1c5c1966771777f324d838094f89
Kyle Cole <54221202+cakeholeDC@users.noreply.github.com>
2022-09-14 18:12:29 -0400

    ⚙️  Introduce CAMERA_STATUS var

--- logger.py
+++ logger.py
@@ -9,3 +9,14 @@ def write_light_status(status):
     log_file.write(str(status))
     log_file.write("\n")
     log_file.close()
+
+
+CAMERA_STATUS = os.getenv('CAMERA_STATUS')
+def write_camera_status(status):
+    '''
+    Writes `True` or `False` to the local log file
+    '''
+    log_file = open(CAMERA_STATUS, "w") # pylint: disable=[W1514, R1732]
+    log_file.write(str(status))
+    log_file.write("\n")
+    log_file.close()
