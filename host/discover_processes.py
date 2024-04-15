f90e8f61c728ba9aa6f6a7e0aca9fcf0330db4fd
Kyle Cole <54221202+cakeholeDC@users.noreply.github.com>
2024-04-15 10:49:07 -0700

    Cleanup old files

--- host/discover_processes.py
+++
@@ -1,4 +0,0 @@
-from host.actions import discover_process_names
-
-if __name__ == "__main__":
-    discover_process_names()
--- host/get_vda_power_state.py
+++
@@ -1,5 +0,0 @@
-from host.actions import get_and_log_vdc_status
-
-if __name__ == "__main__":
-    state = get_and_log_vdc_status()
-    print(state)
