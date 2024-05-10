2eca87377d8b0597aae6fa048f5bf5e29c4bfa3a
Kyle Cole <54221202+cakeholeDC@users.noreply.github.com>
2024-05-10 10:49:57 -0700

    PYTEST: skip or mock in CI

--- tests/test_hass_api.py
+++ tests/test_hass_api.py
@@ -8,20 +8,20 @@ from hass.api import get_entity_state, toggle_entity_state
 # from mock_data import MOCK_ENTITY_STATE
 
 
-@pytest.mark.skipif(os.environ.get("GITHUB_MOCK_API") == "True")
+@pytest.mark.skipif(
+    os.environ.get("GITHUB_MOCK_API") == "True", reason="Skip when running in CI"
+)
 def test_get_entity_state():
     entity_state = get_entity_state(CONFIG["HASS_ENTITY_ID"])
-    print(entity_state)
+
     # assertions
     assert isinstance(entity_state, dict)
     assert entity_state.keys() == PYTEST["MOCK_ENTITY_STATE"].keys()
-    # assert entity_state["state"] in ["on", "off"]
-    # assert "entity_id" in entity_state.keys()
-    # assert "state" in entity_state.keys()
-    # assert "last_changed" in entity_state.keys()
 
 
-@pytest.mark.skipif(os.environ.get("GITHUB_MOCK_API") != "True")
+@pytest.mark.skipif(
+    os.environ.get("GITHUB_MOCK_API") != "True", reason="Skip when running locally"
+)
 def test_get_entity_state_mocked(mocker):
     # Create a mock response object with a .json() method that returns the mock data
     mock_response = mocker.MagicMock()
@@ -29,20 +29,19 @@ def test_get_entity_state_mocked(mocker):
 
     # Patch 'requests.get' to return the mock response
     mocker.patch("requests.get", return_value=mock_response)
-    print(mock_response)
+
     # Call the function
     entity_state = get_entity_state(CONFIG["HASS_ENTITY_ID"])
-    print(entity_state)
+
     # assertions
     assert isinstance(entity_state, dict)
-    assert entity_state == mock_response
-    # assert entity_state["state"] in ["on", "off"]
-    # assert "entity_id" in entity_state.keys()
-    # assert "state" in entity_state.keys()
-    # assert "last_changed" in entity_state.keys()
+    assert entity_state == PYTEST["MOCK_ENTITY_STATE"]
 
 
-@pytest.mark.skipif(os.environ.get("GITHUB_MOCK_API") == "True")
+# TODO: mock this function in CI
+@pytest.mark.skipif(
+    os.environ.get("GITHUB_MOCK_API") == "True", reason="Skip when running in CI"
+)
 def test_toggle_entity_state():
     # Create a mock response object with a .json() method that returns the mock data
     # mock_response = mocker.MagicMock()
