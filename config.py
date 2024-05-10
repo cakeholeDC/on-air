1ef9fab4790ef85a5c6c575670a9203d24332745
Kyle Cole <54221202+cakeholeDC@users.noreply.github.com>
2024-05-10 11:59:34 -0700

    Improve HASS / MOCK API coverage

--- config.py
+++ config.py
@@ -23,19 +23,3 @@ CONFIG = {
     "SHORTCUT_OFF": os.getenv("SHORTCUT_OFF"),
     "SHORTCUT_STATE": os.getenv("SHORTCUT_STATE"),
 }
-
-PYTEST = {
-    "MOCK_ENTITY_STATE": {
-        "entity_id": "switch.mock",
-        "state": "off",
-        "attributes": {"friendly_name": "mocked-switch"},
-        "last_changed": "2024-05-09T23:50:54.580092+00:00",
-        "last_reported": "2024-05-09T23:50:54.580092+00:00",
-        "last_updated": "2024-05-09T23:50:54.580092+00:00",
-        "context": {
-            "id": "m0ck3dD3v1c31d",
-            "parent_id": "null",
-            "user_id": "m0ck3du53r1d",
-        },
-    }
-}
--- tests/test_hass_api.py
+++ tests/test_hass_api.py
@@ -2,30 +2,30 @@ import os
 
 import pytest
 
-from config import CONFIG, PYTEST
+from config import CONFIG
 from hass.api import get_entity_state, toggle_entity_state
-
-# from mock_data import MOCK_ENTITY_STATE
+from hass.fixtures import MOCK_API
 
 
 @pytest.mark.skipif(
-    os.environ.get("GITHUB_MOCK_API") == "True", reason="Skip when running in CI"
+    os.environ.get("MOCK_API") == "True",
+    reason="Skip when running in CI",
 )
 def test_get_entity_state():
     entity_state = get_entity_state(CONFIG["HASS_ENTITY_ID"])
-
     # assertions
     assert isinstance(entity_state, dict)
-    assert entity_state.keys() == PYTEST["MOCK_ENTITY_STATE"].keys()
+    assert entity_state.keys() == MOCK_API["STATES_ENTITY_RESPONSE"].keys()
 
 
 @pytest.mark.skipif(
-    os.environ.get("GITHUB_MOCK_API") != "True", reason="Skip when running locally"
+    os.environ.get("MOCK_API") != "True",
+    reason="Skip when running locally",
 )
 def test_get_entity_state_mocked(mocker):
     # Create a mock response object with a .json() method that returns the mock data
     mock_response = mocker.MagicMock()
-    mock_response.json.return_value = PYTEST["MOCK_ENTITY_STATE"]
+    mock_response.json.return_value = MOCK_API["STATES_ENTITY_RESPONSE"]
 
     # Patch 'requests.get' to return the mock response
     mocker.patch("requests.get", return_value=mock_response)
@@ -35,24 +35,53 @@ def test_get_entity_state_mocked(mocker):
 
     # assertions
     assert isinstance(entity_state, dict)
-    assert entity_state == PYTEST["MOCK_ENTITY_STATE"]
+    assert entity_state == MOCK_API["STATES_ENTITY_RESPONSE"]
 
 
-# TODO: mock this function in CI
 @pytest.mark.skipif(
-    os.environ.get("GITHUB_MOCK_API") == "True", reason="Skip when running in CI"
+    os.environ.get("MOCK_API") == "True",
+    reason="Skip when running in CI",
 )
 def test_toggle_entity_state():
-    # Create a mock response object with a .json() method that returns the mock data
-    # mock_response = mocker.MagicMock()
-    # mock_response.json.return_value = PYTEST["MOCK_ENTITY_STATE"]
-
-    # # Patch 'requests.get' to return the mock response
-    # mocker.patch("requests.get", return_value=mock_response)
-    # print(mock_response)
-    # Call the function
     toggled_state = toggle_entity_state(CONFIG["HASS_ENTITY_ID"])
-    print(toggled_state)
     # assertions
     assert isinstance(toggled_state[0], dict)
     assert toggled_state[0]["state"] in ["on", "off"]
+
+
+def test_get_entity_state_with_ci_mock(mocker):
+    if os.environ.get("MOCK_API") == "True":
+        # Create a mock response object with a .json() method that returns the mock data
+        mock_response = mocker.MagicMock()
+        mock_response.json.return_value = MOCK_API["STATES_ENTITY_RESPONSE"]
+
+        # Patch 'requests.get' to return the mock response
+        mocker.patch("requests.get", return_value=mock_response)
+
+    # Call the function
+    entity_state = get_entity_state(CONFIG["HASS_ENTITY_ID"])
+
+    # assertions
+    assert isinstance(entity_state, dict)
+    assert entity_state.keys() == MOCK_API["STATES_ENTITY_RESPONSE"].keys()
+    if os.environ.get("MOCK_API") == "True":
+        assert entity_state == MOCK_API["STATES_ENTITY_RESPONSE"]
+
+
+def test_toggle_entity_state_with_ci_mock(mocker):
+    if os.environ.get("MOCK_API") == "True":
+        # Create a mock response object with a .json() method that returns the mock data
+        mock_response = mocker.MagicMock()
+        mock_response.json.return_value = MOCK_API["SERVICES_TOGGLE_RESPONSE"]
+
+        # Patch 'requests.get' to return the mock response
+        mocker.patch("requests.post", return_value=mock_response)
+
+    # Call the function
+    toggled_state = toggle_entity_state(CONFIG["HASS_ENTITY_ID"])
+
+    # assertions
+    assert isinstance(toggled_state[0], dict)
+    assert toggled_state[0].keys() == MOCK_API["SERVICES_TOGGLE_RESPONSE"][0].keys()
+    if os.environ.get("MOCK_API") == "True":
+        assert toggled_state == MOCK_API["SERVICES_TOGGLE_RESPONSE"]
