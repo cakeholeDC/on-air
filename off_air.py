21ded749e4f71414aa63e8c0388e17b240e43c55
Kyle Cole <54221202+cakeholeDC@users.noreply.github.com>
2024-05-13 08:21:08 -0700

    🪵 Improve logging

--- off_air.py
+++ off_air.py
@@ -2,11 +2,18 @@ from config import CONFIG
 from host.cache import update_cache
 from host.colors import BLACK, COLOR_NONE, DARK_GRAY
 from host.control import offair
+from host.logger import logger
 
-if __name__ == "__main__":
+
+def run():
+    logger.info("💻 Manually invoked: 'offair'")
     offair()
     update_cache(CONFIG["DEVICE_CACHE"], False)
 
     print(f"{DARK_GRAY}------------------")
     print(f"{DARK_GRAY}| --- {BLACK}ON AIR{DARK_GRAY} --- |")
     print(f"{DARK_GRAY}------------------{COLOR_NONE}")
+
+
+if __name__ == "__main__":
+    run()
--- on_air.py
+++ on_air.py
@@ -2,11 +2,18 @@ from config import CONFIG
 from host.cache import update_cache
 from host.colors import COLOR_NONE, GREEN, RED
 from host.control import onair
+from host.logger import logger
 
-if __name__ == "__main__":
+
+def run():
+    logger.info("💻 Manually invoked 'onair'")
     onair()
     update_cache(CONFIG["DEVICE_CACHE"], True)
 
     print(f"{GREEN}------------------")
     print(f"{GREEN}| --- {RED}ON AIR {GREEN}--- |")
     print(f"{GREEN}------------------{COLOR_NONE}")
+
+
+if __name__ == "__main__":
+    run()
